0405df0af889e61160ef5913d09012c3407449d1
LuizHFJesus <luiz.falomes@gmail.com>
2022-08-08 21:10:03 -0300

    🧱 Change status and bottom bar color

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,13 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:moleculas_ar/app_widget.dart';
+import 'package:moleculas_ar/shared/theme/app_theme.dart';
 
 void main() {
+  SystemChrome.setSystemUIOverlayStyle(SystemUiOverlayStyle(
+    systemNavigationBarColor: AppTheme.colors.background,
+    statusBarColor: AppTheme.colors.background,
+  ));
+
   runApp(const AppWidget());
 }
