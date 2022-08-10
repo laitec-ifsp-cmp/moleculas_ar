f32a58778e647ce5adb14cdb0ffddb2d207f92d5
LuizHFJesus <luiz.falomes@gmail.com>
2022-08-09 22:00:00 -0300

    test

--- lib/modules/splash/splash_page.dart
+++ lib/modules/splash/splash_page.dart
@@ -7,7 +7,7 @@ class SplashPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    Future.delayed(Duration(seconds: 3)).then(
+    Future.delayed(Duration(seconds: 10)).then(
         (value) => Navigator.pushReplacementNamed(context, "/on_boarding"));
 
     return Scaffold(
@@ -18,7 +18,7 @@ class SplashPage extends StatelessWidget {
           child: Column(
             mainAxisAlignment: MainAxisAlignment.center,
             children: [
-              Image.asset("assets/images/logo.png"),
+              Image.asset(AppRes.images.logo),
               SizedBox(height: AppRes.dimens.defaultVerticalMargin),
               Text(AppRes.strings.appName).splashText,
             ],
