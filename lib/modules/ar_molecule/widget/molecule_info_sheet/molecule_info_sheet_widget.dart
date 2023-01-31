77ebc058df602de60a01863f47449aa2533bc505
LuizHFJesus <luiz.falomes@gmail.com>
2023-01-30 21:11:40 -0300

    🌠 Add no scroll physics for molecule info sheet

--- lib/modules/ar_molecule/widget/molecule_info_sheet/molecule_info_sheet_widget.dart
+++ lib/modules/ar_molecule/widget/molecule_info_sheet/molecule_info_sheet_widget.dart
@@ -27,41 +27,51 @@ class MoleculeInfoSheetWidget extends StatelessWidget {
             color: AppTheme.colors.background,
             borderRadius: const BorderRadius.vertical(top: Radius.circular(30)),
           ),
-          child: SingleChildScrollView(
-            physics: const ClampingScrollPhysics(),
-            controller: scrollController,
-            child: Padding(
-              padding: EdgeInsets.symmetric(
-                vertical: AppRes.dimens.defaultVerticalMargin,
-                horizontal: AppRes.dimens.appHorizontalMargin,
-              ),
-              child: Column(children: [
-                if (structuralFormulaImagePath.isNotEmpty)
-                  Container(
-                    height: 5,
-                    width: 40,
-                    decoration: BoxDecoration(
-                      color: AppTheme.colors.light,
-                      borderRadius: BorderRadius.circular(5),
-                    ),
-                  ),
-                if (structuralFormulaImagePath.isNotEmpty)
-                  SizedBox(height: AppRes.dimens.defaultVerticalMargin),
-                Text(
-                  moleculeName,
-                  style: AppTheme.textStyles.moleculeName,
-                  textAlign: TextAlign.center,
+          child: ScrollConfiguration(
+            behavior: NoScrollPhysics(),
+            child: SingleChildScrollView(
+              controller: scrollController,
+              child: Padding(
+                padding: EdgeInsets.symmetric(
+                  vertical: AppRes.dimens.defaultVerticalMargin,
+                  horizontal: AppRes.dimens.appHorizontalMargin,
                 ),
-                Text(moleculeFormula).moleculeFormula,
-                if (structuralFormulaImagePath.isNotEmpty)
-                  SizedBox(height: AppRes.dimens.largeMargin),
-                if (structuralFormulaImagePath.isNotEmpty)
-                  Image.asset(structuralFormulaImagePath, height: 145)
-              ]),
+                child: Column(children: [
+                  if (structuralFormulaImagePath.isNotEmpty)
+                    Container(
+                      height: 5,
+                      width: 40,
+                      decoration: BoxDecoration(
+                        color: AppTheme.colors.light,
+                        borderRadius: BorderRadius.circular(5),
+                      ),
+                    ),
+                  if (structuralFormulaImagePath.isNotEmpty)
+                    SizedBox(height: AppRes.dimens.defaultVerticalMargin),
+                  Text(
+                    moleculeName,
+                    style: AppTheme.textStyles.moleculeName,
+                    textAlign: TextAlign.center,
+                  ),
+                  Text(moleculeFormula).moleculeFormula,
+                  if (structuralFormulaImagePath.isNotEmpty)
+                    SizedBox(height: AppRes.dimens.largeMargin),
+                  if (structuralFormulaImagePath.isNotEmpty)
+                    Image.asset(structuralFormulaImagePath, height: 145)
+                ]),
+              ),
             ),
           ),
         );
       },
     );
+  }
+}
+
+class NoScrollPhysics extends ScrollBehavior {
+  @override
+  Widget buildOverscrollIndicator(
+      BuildContext context, Widget child, ScrollableDetails details) {
+    return child;
   }
 }
